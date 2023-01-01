53b437685e078e38ed59b542c45475bafc013825
azuki774 <azuki774s@gmail.com>
2023-01-02 02:47:03 +0900

    add test/migration

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 SHELL=/bin/bash
 
-.PHONY: build
+.PHONY: build start
 build:
 	docker build -t sql-migrate-git -f build/Dockerfile .
+
+start:
+	docker compose -f deployment/compose-local.yml up
--- scripts/main.sh
+++ scripts/main.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+BIN=/usr/local/bin/sql-migrate
+
+GIT_REPO=${GIT_REPO}
+MIGRATE_DIR=${MIGRATE_DIR}
+MIGRATE_ENV=${MIGRATE_ENV}
+
+echo "migrate start"
+echo "Git Repository: ${GIT_REPO}"
+echo "migrate directory: ${MIGRATE_DIR}"
+echo "migrate env: ${MIGRATE_ENV}"
