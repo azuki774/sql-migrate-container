544859b3ccfb79e181f7ca2e17ea6ef83dbba211
azuki774 <azuki774s@gmail.com>
2023-01-02 03:39:59 +0900

    fix SQL

--- test/migration/db/001.sql
+++ test/migration/db/001.sql
@@ -3,7 +3,7 @@ CREATE TABLE `Record` (
   `id` int NOT NULL AUTO_INCREMENT,
   `created_at` datetime  default current_timestamp,
   `updated_at` timestamp default current_timestamp on update current_timestamp,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`)
 );
 
 -- +migrate Down
